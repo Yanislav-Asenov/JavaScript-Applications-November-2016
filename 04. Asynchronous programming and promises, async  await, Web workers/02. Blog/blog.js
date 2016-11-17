85a126cefb9eaac9b67abad54441a9704edfcd39
Yanislav-Asenov <yanislav.asenov@gmail.com>
2016-11-18 01:39:11 +0200

    All exercises for this lecture done

---
+++ 04. Asynchronous programming and promises, async  await, Web workers/02. Blog/blog.js
@@ -0,0 +1,100 @@
+function attachEvents () {
+    const baseUrl = 'https://baas.kinvey.com/appdata/kid_B1iu1jo-l';
+    const appId = 'kid_B1iu1jo-l';
+    const appSecret = 'abde59139e534909bbc8d06a2e13696f';
+    const username = 'guest';
+    const password = 'guest';
+    const base64auth = btoa(`${username}:${password}`);
+    const authHeaders = {
+        Authorization: 'Basic ' + base64auth
+    };
+
+    let postsSelectList = $('#posts');
+    postsSelectList.prop('selectedIndex', '-1');
+    let postTitleHeader = $('#post-title');
+    let postBodyParagraph = $('#post-body');
+    let commentsUl = $('#post-comments');
+
+    $('#btnLoadPosts').on('click', loadPosts);
+    $('#btnViewPost').on('click', getPostDetails);
+
+    function getPostDetails () {
+        let postId = postsSelectList.val();
+        let getPostRequest = {
+            method: 'GET',
+            headers: authHeaders,
+            url: `${baseUrl}/posts/${postId}`
+        };
+
+        let getPostCommentsRequest = {
+            method: 'GET',
+            headers: authHeaders,
+            url: `${baseUrl}/comments/?query={"post_id":"${postId}"}`
+        };
+
+        // Load post title and body
+        $.ajax(getPostRequest)
+            .then(displayPostDetails)
+            .catch(displayError);
+        
+        // Load post comments
+        $.ajax(getPostCommentsRequest)
+            .then(displayPostComments)
+            .catch(displayError);
+    }
+
+    function displayPostComments (comments) {
+        commentsUl.empty();
+        for (let comment of comments) {
+            let commentLi = $('<li>').text(comment.body);
+            commentsUl.append(commentLi);
+        }
+    }
+
+    function displayPostDetails (post) {
+        postTitleHeader.text(post.title);
+        postBodyParagraph.text(post.body);
+    }
+
+    function loadPosts () {
+        let getPostsRequest = {
+            method: 'GET',
+            headers: authHeaders,
+            url: `${baseUrl}/posts`
+        };
+
+        $.ajax(getPostsRequest)
+            .then(addPostsToSelectList)
+            .catch(displayError);
+    }
+
+    function addPostsToSelectList (posts) {
+        postsSelectList.empty();
+        for (let post of posts) {
+            let option = $('<option>')
+                            .val(post._id)
+                            .text(post.title);
+
+            postsSelectList.append(option);
+        }
+
+        let errorOption = $('<option value="Test pri nevaliden post">Nevaliden post(Test za greshka)</option>');
+        postsSelectList.append(errorOption);
+    }
+
+    function displayError (err) {
+        postTitleHeader.text('');
+        postBodyParagraph.text('');
+        console.log('Error: ' + err.message);
+        let errorDiv = $('<div>')
+                .css({
+                    background: 'red',
+                    color: 'white',
+                    fontWeight: 'bold',
+                    fontSize: '20',
+                })
+                .text('Error: ' + err.statusText)
+                .fadeOut(5000);
+        $('body').prepend(errorDiv);
+    }
+}
