aa38501f8b765c4e344da7fabfda481f16f9174d
Yanislav-Asenov <yanislav.asenov@gmail.com>
2016-11-23 05:23:03 +0200

    Added loader while waiting the ajax request

--- 07. Creating Single-Page App with jQuery, AJAX and Kinvey (without framework)/scripts/app.js
+++ 07. Creating Single-Page App with jQuery, AJAX and Kinvey (without framework)/scripts/app.js
@@ -89,16 +89,18 @@ function startApp () {
 
     // Bind the info / error boxes: hide on click
     errorBox.click(function() {
-        $(this).fadeOut();
+        $("#errorBox").slideUp();
     });
 
     // Attach AJAX "loading" event listener
     $(document).on({
         ajaxStart: function () {
-            loadingBox.show(); 
+            $('main').hide();
+             $('#loader').show();
         },
-        ajaxStop: function () { 
-            loadingBox.hide(); 
+        ajaxStop: function () {
+             $('#loader').hide();
+              $('main').fadeIn(500);
         }
     });
 
@@ -250,7 +252,7 @@ function startApp () {
 
     function showError (errorMsg) {
         errorBox.text("Error: " + errorMsg);
-        errorBox.show();
+        errorBox.slideDown();
     }
 
     function logoutUser () { 
@@ -373,6 +375,7 @@ function startApp () {
             .then(function () {
                 showInfo('Book successfully created');
             });
+
     }
 
     function editBook (event) {
@@ -425,3 +428,22 @@ function startApp () {
             });
     }
 }
+
+// attach loader to the body
+$(document).ready( function () {
+  var id = 'loader', fill = 'red',
+      size = 20, radius = 3, duration = 1000,
+      maxOpacity = 0.6, minOpacity = 0.15;
+  $('<svg id="'+id+'" width="'+(size*3.5)+'" height="'+size+'">' + 
+   		'<rect width="'+size+'" height="'+size+'" x="0" y="0" rx="'+radius+'" ry="'+radius+'" fill="'+fill+'" fill-opacity="'+maxOpacity+'">' + 
+   			'<animate attributeName="opacity" values="1;'+minOpacity+';1" dur="'+duration+'ms" repeatCount="indefinite"/>' + 
+   		'</rect>' + 
+    	'<rect width="'+size+'" height="'+size+'" x="'+(size*1.25)+'" y="0" rx="'+radius+'" ry="'+radius+'" fill="'+fill+'" fill-opacity="'+maxOpacity+'">' + 
+    		'<animate attributeName="opacity" values="1;'+minOpacity+';1" dur="'+duration+'ms" begin="'+(duration/4)+'ms" repeatCount="indefinite"/>' + 
+    	'</rect>' + 
+    	'<rect width="'+size+'" height="'+size+'" x="'+(size*2.5)+'" y="0" rx="'+radius+'" ry="'+radius+'" fill="'+fill+'" fill-opacity="'+maxOpacity+'">' + 
+    		'<animate attributeName="opacity" values="1;'+minOpacity+';1" dur="'+duration+'ms" begin="'+(duration/2)+'ms" repeatCount="indefinite"/>' + 
+    	'</rect>' + 
+   	'</svg>').appendTo('body');
+    $('#loader').hide();
+});
